14455166ec2bd682a236bba4b4e28732ce6ec4e8
vishwas anand <vishwasanand.cse@gmail.com>
2021-06-07 12:18:30 +0530

    removed var from conifg and added directly to truffle config

--- config.js
+++ config.js
@@ -2,6 +2,5 @@ module.exports = {
     TOTAL_SUPPLY: 50000000,
     TOKEN_NAME: "Hypersign Identity Token",
     TOKEN_SYMBOL: "HID",
-    DECIMALS: 18,
-    REACT_APP_INFURA_KEY: process.env.INFURA_KEY
+    DECIMALS: 18
 }
