3f9eee591dd11836475405588c5c93f94d6532a3
vishwas anand <vishwasanand.cse@gmail.com>
2021-06-25 11:12:09 +0530

    removed unwanted code

--- config.js
+++ config.js
@@ -9,8 +9,8 @@ module.exports = {
     DECIMALS: 18,
     vesting:{
         seedAndPrivate : {
-            token : "0x9491fae7A7CAAF993EAcB34a8Fc4848c9d1AB8C6", // HID token address
-            beneficiary : "0x31beAb2d3cCD79b589A181f3fAb5fc5B6530d2E8", // address of beneficiary
+            token : "", // HID token address
+            beneficiary : "0x81a32051A7c6417E79591e239869F4179429563f", // address of beneficiary
             startTime :  Math.ceil((new Date().getTime()) / 1000),    // start time in seconds (epoch time)
             cliffDuration : (2 * MINUTE), // cliff duration in seconds
             waitDuration : (2 * MINUTE), // wait duration after cliff in seconds
